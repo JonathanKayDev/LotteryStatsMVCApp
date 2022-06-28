f5bd94b34805105494bb8c0a8dd169f2c5bb0f34
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-06-28 21:08:37 +0100

    create WebConnector service

---
+++ Services/WebConnector.cs
@@ -0,0 +1,6 @@
+﻿namespace LotteryStatsMVCApp.Services
+{
+    public class WebConnector
+    {
+    }
+}
---
+++ Services/WebConnectorProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace LotteryStatsMVCApp.Services
+{
+    public static class WebConnectorProcessor
+    {
+        public static string webPath = "https://www.national-lottery.co.uk/results/";
+
+        public static string FullWebPath(string webFile)
+        {
+            return $"{webPath}{ webFile }";
+        }
+
+        public static List<string> LoadWeb(this string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+
+            List<string> webCsv = new List<string>();
+
+            while (!sr.EndOfStream)
+            {
+                webCsv.Add(sr.ReadLine());
+            }
+
+            webCsv.RemoveAt(0); // remove header row
+
+            return webCsv;
+
+        }
+    }
+}
